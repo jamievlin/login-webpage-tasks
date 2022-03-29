698abe689045e2b4025db19b4bcebd4e4e722e7d
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2022-03-28 21:42:05 -0600

    TSX: Add basic frontend.

--- frontend/app.tsx
+++ frontend/app.tsx
@@ -1,15 +1,66 @@
-import React from 'react';
+import React, {ChangeEventHandler} from 'react';
 import ReactDOM from 'react-dom';
+import { Buffer } from 'buffer';
 
-class App extends React.Component {
+type AppState = {
+    username: string,
+    password: string
+}
+
+class App extends React.Component<{}, AppState> {
     constructor(prop: {}) {
         super(prop);
+        this.state = {
+            username: "",
+            password: ""
+        }
+
+        // functions
+        this.onUsernameTextChange = this.onUsernameTextChange.bind(this);
+        this.onPasswordTextChange = this.onPasswordTextChange.bind(this);
+        this.onLoginButtonClick = this.onLoginButtonClick.bind(this);
+    }
+
+    onUsernameTextChange(e: React.ChangeEvent<HTMLInputElement>) {
+        this.setState({username: e.target.value});
+    }
+
+    onPasswordTextChange(e: React.ChangeEvent<HTMLInputElement>) {
+        this.setState({password: e.target.value})
+    }
+
+    async login() {
+        const loginstr = Buffer.from(`${this.state.username}:${this.state.password}`).toString('base64');
+        const rsp = await fetch(`${window.location.origin}/api/testlogin`, {
+            method: "GET",
+            headers: new Headers({
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': `Basic ${loginstr}`
+            })
+        });
+        const ret = await rsp.json()
+        if (ret.valid) {
+            alert('login success!')
+        } else {
+            alert('login failure!')
+        }
+    }
+
+    onLoginButtonClick() {
+        const _ = this.login();
     }
 
     render() {
         return (
             <>
-                Hello from React!
+                Username:
+                <input type="text" value={this.state.username} name="username" onChange={this.onUsernameTextChange} />
+                <br />
+                Password:
+                <input type="password" value={this.state.password} name="pwd" onChange={this.onPasswordTextChange}/>
+                <br />
+                <button onClick={this.onLoginButtonClick}>Login</button>
             </>
         );
     }
