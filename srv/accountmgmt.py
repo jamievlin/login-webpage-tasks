e313e51870627281b2bbfc227cfedbc199874ae1
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2022-04-03 02:34:55 -0600

    PY: Add session management functions to accountmgmt.py.

--- srv/accountmgmt.py
+++ srv/accountmgmt.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
+import datetime
 import mariadb
+import secrets
+import typing as ty
+import ipaddress
 from passlib.hash import argon2
 
 from .db_helper import db_conn, fetch_one
@@ -23,6 +27,74 @@ def create_user(username: str, password: str):
                 print(f'Error: {e}')
 
 
+def get_userid(username: str) -> ty.Optional[int]:
+    query = """
+    SELECT user_id
+    FROM login_webpage.user_login
+    WHERE user_name = %s
+    """
+    ret = fetch_one(query, (username, ))
+    if ret is None:
+        return None
+    userid, = ret
+    return userid
+
+
+def get_username(user_id: int) -> ty.Optional[str]:
+    query = """
+    SELECT user_name
+    FROM login_webpage.user_login
+    WHERE user_id = %d
+    """
+    ret = fetch_one(query, (user_id, ))
+    if ret is None:
+        return None
+    username, = ret
+    return username
+
+
+def create_session(
+        username: str,
+        ip_addr: ty.Union[ipaddress.IPv4Address, ipaddress.IPv6Address]) \
+        -> ty.Optional[ty.Tuple[bytes, datetime.datetime]]:
+    session_bytes = secrets.token_bytes(32)
+    expiry = datetime.datetime.now() + datetime.timedelta(minutes=30)
+    query = """
+    INSERT INTO login_webpage.sessions
+    (session_id, user_id, expiry, ip_addr)
+    VALUES (%s, %d, %s, %s)
+    """
+    userid = get_userid(username)
+    if userid is None:
+        return None
+
+    with db_conn(True) as cur:
+        try:
+            cur.execute(query, (session_bytes, userid, expiry, ip_addr.packed))
+        except mariadb.Error as e:
+            print(f'error: {e}')
+            return None
+    return session_bytes, expiry
+
+
+def clear_all_session(username: str):
+    userid = get_userid(username)
+    if userid is None:
+        return
+
+    query = """
+    DELETE FROM login_webpage.sessions
+    WHERE user_id = %d
+    """
+
+    with db_conn(True) as cur:
+        try:
+            cur.execute(query, (userid,))
+        except mariadb.Error as e:
+            print(f'error: {e}')
+            return None
+
+
 def change_password(username: str, new_password: str):
     new_hash = argon2.hash(new_password)
     query = """
@@ -36,6 +108,35 @@ def change_password(username: str, new_password: str):
             cur.execute(query, {'pwdhash': new_hash, 'usr': username})
         except mariadb.Error as e:
             print(f'oops... {e}')
+
+
+def verify_session(session: bytes) -> \
+        ty.Optional[ty.Tuple[str, datetime.datetime]]:
+    query = """
+    SELECT user_id, expiry
+    FROM login_webpage.sessions
+    WHERE session_id = %s AND expiry >= NOW()
+    """
+    ret = fetch_one(query, (session, ))
+    if ret is None:
+        return None
+    userid, expiry = ret
+
+    new_expiry = expiry + datetime.timedelta(minutes=30)
+    username = get_username(userid)
+    update_expiry = """
+    UPDATE login_webpage.sessions
+    SET expiry = %s
+    WHERE session_id = %s
+    """
+
+    with db_conn(True) as cur:
+        try:
+            cur.execute(update_expiry, (new_expiry, session))
+        except mariadb.Error as e:
+            print(f'error: {e}')
+            return username, expiry
+    return username, new_expiry
 
 
 def verify_login(username: str, password: str) -> bool:
