a97669cf902cf76d49db6aa87636a1768f5b90f1
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2022-04-10 02:02:25 -0600

    PY: Use http.HTTPStatus to return response codes.

--- endpoints/login.py
+++ endpoints/login.py
@@ -7,23 +7,27 @@ from utils import auth
 from ipaddress import IPv4Address
 from srv import accountmgmt as am
 
+from http import HTTPStatus
+
 login_page = Blueprint('login_endpoint', __name__, template_folder='templates')
 
 
 @login_page.route('/api/testlogin', methods=['GET'])
 def test_login():
     usr = auth.get_username_by_pw_or_session()
-    return jsonify(valid=usr is not None), 200
+    return jsonify(valid=usr is not None), HTTPStatus.OK
 
 
 @login_page.route('/api/login', methods=['POST'])
 def login():
     usr = auth.get_username_auth()
     if usr is None:
-        return jsonify(message='User login failed!'), 401
+        return jsonify(message='User login failed!'), \
+               HTTPStatus.UNAUTHORIZED
     ret = am.create_session(usr, IPv4Address(request.remote_addr))
     if ret is None:
-        return jsonify(message='Cannot create user session'), 401
+        return jsonify(message='Cannot create user session'), \
+               HTTPStatus.UNAUTHORIZED
     session, exp = ret
 
     retval = {
--- endpoints/usermgmt.py
+++ endpoints/usermgmt.py
@@ -4,7 +4,7 @@ from flask import (
     Blueprint, jsonify, make_response
 )
 from utils import auth
-
+from http import HTTPStatus
 
 user_mgmt = Blueprint('user_mgmt', __name__, template_folder='templates')
 
@@ -14,5 +14,6 @@ def userdata():
     usr = auth.get_username_by_session()
     if usr is None:
         return make_response(
-            401, jsonify(message='Invalid or expired session!'))
-    return jsonify(username=usr), 200
+            HTTPStatus.UNAUTHORIZED,
+            jsonify(message='Invalid or expired session!'))
+    return jsonify(username=usr), HTTPStatus.OK
