4e17f163e9daf4b03ecf1cad76f519e87b17ed4b
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2022-04-10 02:13:14 -0600

    APP: Add basic logout button.

--- frontend/userinfo/userinfo.tsx
+++ frontend/userinfo/userinfo.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {MouseEvent} from "react";
 import '@cds/core/global.min.css'
 import './userinfo.css';
 import {Configuration, DefaultApi} from "../apis";
@@ -9,23 +9,27 @@ interface UserInfoState {
     username: string
 }
 
+function returnToLogin() {
+    window.location.href = `${window.location.origin}/`;
+}
+
 class UserInfo extends React.Component<{}, UserInfoState> {
     constructor(props: {}) {
         super(props);
         this.state = {
             api: undefined,
             username: ''
-        }
+        };
 
-        this.setComponentValues = this.setComponentValues.bind(this)
+        this.setComponentValues = this.setComponentValues.bind(this);
+        this.onLogoutBtnClick = this.onLogoutBtnClick.bind(this);
     }
 
     componentDidMount() {
         const session = sessionStorage.getItem('session')
         if (session === null) {
-            console.error('not logged in... ')
-            window.location.href = `${window.location.origin}/`
-
+            console.error('not logged in... ');
+            returnToLogin();
         } else {
             const defaultApiCfg = new Configuration({
                 accessToken: session
@@ -44,9 +48,23 @@ class UserInfo extends React.Component<{}, UserInfoState> {
         })
     }
 
+    onLogoutBtnClick(ev: MouseEvent<HTMLButtonElement>) {
+        console.log('hello?')
+        this.state.api?.deleteSession().then(() => {
+            sessionStorage.removeItem('session');
+            returnToLogin();
+        }).catch(err => {
+            console.log('???')
+            console.log(`err: ${err}`)
+        })
+    }
+
     render() {
         return (
-            <>Welcome, {this.state.username}</>
+            <>
+            Welcome, {this.state.username}.
+            <button onClick={this.onLogoutBtnClick}>logout</button>
+            </>
         );
     }
 }
