c7507291d289047b0d3c9830d67b06a80aed9de1
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2022-04-16 17:59:16 -0600

    SQL: Add MySQL connector for IDE hinting.

--- srv/db_helper.py
+++ srv/db_helper.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env python3
+import mysql.connector
 import mariadb
 from contextlib import contextmanager
 import typing as ty
+
+
+class DbCursor:
+    def __init__(self, commit=False):
+        self._connection = mysql.connector.MySQLConnection(
+            user='default', password='password',
+            host='localhost', port=3306,
+            database='login_webpage'
+        )
+        self.commit = commit
+
+    def __enter__(self):
+        return self._connection.cursor()
+
+    def __exit__(self, ex_type, ex_val, ex_tb):
+        if self.commit:
+            self._connection.commit()
+        self._connection.close()
+        return False
 
 
 @contextmanager
