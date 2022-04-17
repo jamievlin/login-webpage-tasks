d06460962ba10aed1f974b5fb8bbfa27a3fc565f
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2022-04-16 19:01:15 -0600

    API: Add userid to /userdata endpoint.

--- endpoints/usermgmt.py
+++ endpoints/usermgmt.py
@@ -4,6 +4,7 @@ from flask import (
     Blueprint, jsonify, make_response
 )
 from utils import auth
+from srv import accountmgmt as am
 from http import HTTPStatus
 
 user_mgmt = Blueprint('user_mgmt', __name__, template_folder='templates')
@@ -12,8 +13,9 @@ user_mgmt = Blueprint('user_mgmt', __name__, template_folder='templates')
 @user_mgmt.route('/api/userdata')
 def userdata():
     usr = auth.get_username_by_session()
+    user_id = am.get_userid(usr)
     if usr is None:
         return make_response(
             HTTPStatus.UNAUTHORIZED,
             jsonify(message='Invalid or expired session!'))
-    return jsonify(username=usr), HTTPStatus.OK
+    return jsonify(username=usr, userid=user_id), HTTPStatus.OK
